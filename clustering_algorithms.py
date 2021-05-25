ea03ccad62f298607eeb0a825d5165cb39c87e93
Lars <huth@mailbox.org>
2021-05-25 14:20:20 +0200

    Code formatting using black, indcluding sources to code-snippets

--- clustering_algorithms.py
+++ clustering_algorithms.py
@@ -1,50 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 import dataset_tranformations
 
 from sklearn.cluster import DBSCAN
-from sklearn import metrics
-from sklearn.datasets import make_blobs
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import normalize
-
-import pandas as pd
-
-
-def plotting_dbscan(dataset, labels, x_var='', y_var=''):
-    # Building the label to colour mapping
-    colours = {}
-
-    unique_labels = set(labels)
-    colors = [plt.cm.Spectral(each)
-              for each in np.linspace(0, 1, len(unique_labels))]
-
-    colours[-1] = 'k'
-
-    # Building the colour vector for each data point
-    cvec = [colors[label] for label in labels]
-
-    # For the construction of the legend of the plot
-    if not x_var and y_var:
-        x_var = labels[0]
-        y_var = labels[1]
-
-    r = plt.scatter(dataset[x_var], dataset[y_var], color='r')
-    g = plt.scatter(dataset[x_var], dataset[y_var], color='g')
-    b = plt.scatter(dataset[x_var], dataset[y_var], color='b')
-    k = plt.scatter(dataset[x_var], dataset[y_var], color='k')
-
-    # according to the colour vector defined
-    plt.figure(figsize=(9, 9))
-    plt.scatter(dataset[x_var], dataset[y_var], c=cvec)
-
-    plt.show()
-    return
 
 
 def density_based_spatial_clustering_of_applications_with_noise(dataset):
+    """
+    Performs density-based clustering of applications with noise on datasets transformed as we as a group we agreed
+    upon. This code is based upon
+    https://www.geeksforgeeks.org/implementing-dbscan-algorithm-using-sklearn/
+    """
+
     db = DBSCAN(eps=0.3, min_samples=5).fit(dataset)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
@@ -55,20 +22,24 @@ def density_based_spatial_clustering_of_applications_with_noise(dataset):
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
 
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print('Estimated number of noise points: %d' % n_noise_)
+    print("Estimated number of clusters: %d" % n_clusters_)
+    print("Estimated number of noise points: %d" % n_noise_)
     return db, labels
 
 
-def main(algorithm='dbscan', dataset='happiness and alcohol', pca_bool=True):
+def main(algorithm="dbscan", dataset="happiness and alcohol", pca_bool=True):
     print("pick a god and pray")
 
-    algorithms = {'dbscan': density_based_spatial_clustering_of_applications_with_noise}
-    datasets = {'happiness and alcohol': dataset_tranformations.happiness_alcohol_consumption}
+    algorithms = {"dbscan": density_based_spatial_clustering_of_applications_with_noise}
+    datasets = {
+        "happiness and alcohol": dataset_tranformations.happiness_alcohol_consumption
+    }
 
     x, x_principal = datasets[dataset](pca_bool=pca_bool)
     db, labels = algorithms[algorithm](x_principal)
-    plotting_dbscan(x, labels, 'HappinessScore', 'HDI')
+    dataset_tranformations.plotting_happiness_and_alcohol(
+        x, labels, "HappinessScore", "HDI"
+    )
     return 0
 
 
--- dataset_tranformations.py
+++ dataset_tranformations.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-from sklearn.cluster import DBSCAN
-from sklearn import metrics
-from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
@@ -10,10 +9,18 @@ import pandas as pd
 from sklearn.preprocessing import normalize
 
 
-def happiness_alcohol_consumption(location='/home/lars/Downloads/', pca_bool=True):
-    dataset = location+'HappinessAlcoholConsumption.csv'
+def happiness_alcohol_consumption(location="/home/lars/Downloads/", pca_bool=True):
+    """
+    Transforms the "Happiness and Alcohol Consumption" dataset,
+    https://www.kaggle.com/marcospessotto/happiness-and-alcohol-consumption
+    The reduction of dimensionality is based on the work in the articles
+    https://www.geeksforgeeks.org/implementing-dbscan-algorithm-using-sklearn/
+    https://365datascience.com/tutorials/python-tutorials/pca-k-means/
+    """
+
+    dataset = location + "HappinessAlcoholConsumption.csv"
     x = pd.read_csv(dataset)
-    x = x.drop(['Country', 'Region', 'Hemisphere'], axis=1)
+    x = x.drop(["Country", "Region", "Hemisphere"], axis=1)
 
     # Scaling the data to bring all the attributes to a comparable level
     scaler = StandardScaler()
@@ -27,8 +34,43 @@ def happiness_alcohol_consumption(location='/home/lars/Downloads/', pca_bool=Tru
         pca = PCA(n_components=3)
         x_principal = pca.fit_transform(x_normalized)
         x_principal = pd.DataFrame(x_principal)
-        x_principal.columns = ['P1', 'P2', 'P3']
+        x_principal.columns = ["P1", "P2", "P3"]
     else:
         x_principal = x
 
     return x, x_principal
+
+
+def plotting_happiness_and_alcohol(dataset, labels, x_var="", y_var=""):
+    """
+    Ploting of the "Happiness and Alcohol Consumption" dataset, based on
+    https://www.geeksforgeeks.org/implementing-dbscan-algorithm-using-sklearn/
+    """
+
+    # Building the label to colour mapping
+    colours = {}
+
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+
+    colours[-1] = "k"
+
+    # Building the colour vector for each data point
+    cvec = [colors[label] for label in labels]
+
+    # For the construction of the legend of the plot
+    if not x_var and y_var:
+        x_var = labels[0]
+        y_var = labels[1]
+
+    r = plt.scatter(dataset[x_var], dataset[y_var], color="r")
+    g = plt.scatter(dataset[x_var], dataset[y_var], color="g")
+    b = plt.scatter(dataset[x_var], dataset[y_var], color="b")
+    k = plt.scatter(dataset[x_var], dataset[y_var], color="k")
+
+    # according to the colour vector defined
+    plt.figure(figsize=(9, 9))
+    plt.scatter(dataset[x_var], dataset[y_var], c=cvec)
+
+    plt.show()
+    return
